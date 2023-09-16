3da0b9d4ec44aff8fe4e86d512f02a6199c17d68
Matias <mantitaaa89@gmail.com>
2023-09-16 16:34:19 -0300

    set up urql and first query to projectRegistry subgraph

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -8,6 +8,12 @@ import { configureChains, createClient, useAccount, WagmiConfig } from "wagmi";
 import { goerli, optimism } from "wagmi/chains";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
+import {
+  createClient as urqlClient,
+  Provider,
+  cacheExchange,
+  fetchExchange,
+} from "urql";
 
 const { chains, provider } = configureChains(
   [goerli, optimism],
@@ -24,24 +30,36 @@ const wagmiClient = createClient({
 });
 export { WagmiConfig, RainbowKitProvider };
 
+//Subgraph endpoint url
+const subgraphURL =
+  "https://api.studio.thegraph.com/query/52339/equilibra-goerli/version/latest";
+
+// Create a URQL client
+const client = urqlClient({
+  url: subgraphURL,
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 function App({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = React.useState(false);
   React.useEffect(() => setMounted(true), []);
 
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        modalSize="compact"
-        initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
-        chains={chains}
-      >
-        <NextHead>
-          <title>equilibra</title>
-        </NextHead>
+    <Provider value={client}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          modalSize="compact"
+          initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
+          chains={chains}
+        >
+          <NextHead>
+            <title>equilibra</title>
+          </NextHead>
 
-        {mounted && <Component {...pageProps} />}
-      </RainbowKitProvider>
-    </WagmiConfig>
+          {mounted && <Component {...pageProps} />}
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </Provider>
   );
 }
 
