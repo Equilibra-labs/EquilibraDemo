5e4f52abf6d32d680d8588de9e56eebdda7fc6a1
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-10-04 00:35:59 -0300

    update projectRegistry.json

---
+++ apps/web/abi/index.tsx
@@ -0,0 +1,7 @@
+import ProjectRegistryAbi from "./ProjectRegistry.json";
+
+export const projectRegistry = {
+  abi: ProjectRegistryAbi,
+  contractName: "ProjectRegistry",
+  address: "0x81eBD6Ac50499445C324C5fcc61023881EC4380F",
+};
